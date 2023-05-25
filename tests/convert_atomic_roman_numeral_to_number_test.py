59e148e27cdbf6b3a1fee449624f4d50dc66bd54
Emily Ingram <lmingram@ualberta.ca>
2023-05-25 15:42:41 -0600

    guide future development

--- tests/convert_atomic_roman_numeral_to_number_test.py
+++ tests/convert_atomic_roman_numeral_to_number_test.py
@@ -1,5 +1,8 @@
 from roman_numeral_converter.atomic_conversions import convert_atomic_roman_numeral_to_number
 
+# TODO add error handling
+# TODO add functionality for dealing with capitalized atomic roman numerals (ex. I, V, X, etc.)
+
 def test_that_i_converts_to_1():
     assert convert_atomic_roman_numeral_to_number('i') == 1
 
