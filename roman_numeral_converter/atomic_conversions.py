0e6c8c9d5f97e5f62c75df8b725272460c59fd09
Emily Ingram <lmingram@ualberta.ca>
2023-05-25 15:22:59 -0600

    added new functionality

---
+++ roman_numeral_converter/atomic_conversions.py
@@ -0,0 +1,2 @@
+def convert_atomic_roman_numeral_to_number(roman_numeral: str) -> int:
+    return 1
--- tests/convert_atomic_roman_numeral_to_number_test.py
+++ tests/convert_atomic_roman_numeral_to_number_test.py
@@ -1,1 +1,4 @@
 from roman_numeral_converter.atomic_conversions import convert_atomic_roman_numeral_to_number
+
+def test_that_i_converts_to_1():
+    assert convert_atomic_roman_numeral_to_number('i') == 1
