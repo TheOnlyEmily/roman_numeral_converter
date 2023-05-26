48bef8763aeeb695bedc8eaffe2fca65b833a42e
Emily Ingram <lmingram@ualberta.ca>
2023-05-26 09:35:09 -0600

    added functionality to assist with debugging

--- roman_numeral_converter/atomic_conversions.py
+++ roman_numeral_converter/atomic_conversions.py
@@ -24,3 +24,5 @@ def convert_atomic_roman_numeral_to_number(roman_numeral: str) -> int:
             return 500
         case 'm':
             return 1000
+        case other:
+            raise InvalidAtomicRomanNumeralError(other)
--- tests/convert_atomic_roman_numeral_to_number_test.py
+++ tests/convert_atomic_roman_numeral_to_number_test.py
@@ -1,6 +1,8 @@
-from roman_numeral_converter.atomic_conversions import convert_atomic_roman_numeral_to_number
+import pytest
+import hypothesis.strategies as st
+from hypothesis import given
+from roman_numeral_converter.atomic_conversions import convert_atomic_roman_numeral_to_number, InvalidAtomicRomanNumeralError
 
-# TODO add exception for characters that aren't valid roman numerals 
 # TODO add functionality for dealing with capitalized atomic roman numerals (ex. I, V, X, etc.)
 # TODO add exception for inputs that are more than one character long
 
@@ -24,3 +26,15 @@ def test_that_d_converts_to_500():
 
 def test_that_m_converts_to_1000():
     assert convert_atomic_roman_numeral_to_number('m') == 1000
+
+@given(invalid_chr=st.characters(blacklist_characters=['i', 'v', 'x', 'l', 'c', 'd', 'm']))
+def test_that_characters_that_are_not_valid_automic_roman_numerals_raises_an_expection(invalid_chr):
+    with pytest.raises(InvalidAtomicRomanNumeralError):
+        convert_atomic_roman_numeral_to_number(invalid_chr)
+
+@given(invalid_chr=st.characters(blacklist_characters=['i', 'v', 'x', 'l', 'c', 'd', 'm']))
+def test_that_InvalidAtomicRomanNumeralError_keeps_offending_character_as_invalid_numeral_property(invalid_chr):
+    try:
+        convert_atomic_roman_numeral_to_number(invalid_chr)
+    except InvalidAtomicRomanNumeralError as e:
+        assert e.invalid_numeral == invalid_chr
