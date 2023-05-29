282efbfb2e29552bf6a05897af432690aee4a149
Emily Ingram <lmingram@ualberta.ca>
2023-05-29 10:56:55 -0600

    began adding functionality

--- tests/convert_number_to_atomic_roman_numeral_test.py
+++ tests/convert_number_to_atomic_roman_numeral_test.py
@@ -1,1 +1,5 @@
 from roman_numeral_converter.atomic_conversions import convert_number_to_atomic_roman_numeral
+
+
+def test_1_converts_to_i():
+    assert convert_number_to_atomic_roman_numeral(1) == 'i'
