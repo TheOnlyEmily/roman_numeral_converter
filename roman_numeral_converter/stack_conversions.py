ca45a4211cc934b1f6c7613622e95f0b7a33b719
Emily Ingram <lmingram@ualberta.ca>
2023-06-02 17:07:09 -0600

    added new property

--- roman_numeral_converter/stack_conversions.py
+++ roman_numeral_converter/stack_conversions.py
@@ -1,7 +1,8 @@
 from roman_numeral_converter.atomic_conversions import convert_atomic_roman_numeral_to_number
+from roman_numeral_converter.atomic_conversions import convert_number_to_atomic_roman_numeral
 
 def convert_stack_to_number_list(roman_numeral_stack: str) -> list[int]:
     return list(convert_atomic_roman_numeral_to_number(atomic_rn) for atomic_rn in roman_numeral_stack)
 
 def convert_number_list_to_stack(number_list: list[int]) -> str:
-    return ''.join('i' for _ in number_list)
+    return ''.join(convert_number_to_atomic_roman_numeral(n) for n in number_list)
--- tests/convert_number_list_to_stack_test.py
+++ tests/convert_number_list_to_stack_test.py
@@ -9,3 +9,8 @@ def test_takes_a_list_of_ints_and_returns_a_string(number_list):
 @given(number_list=st.lists(elements=st.sampled_from([1, 5, 10, 50, 100, 500, 1000])))
 def test_returns_roman_numeral_stack_the_same_length_as_list(number_list):
     assert len(number_list) == len(convert_number_list_to_stack(number_list))
+
+@given(number_list1=st.lists(elements=st.sampled_from([1, 5, 10, 50, 100, 500, 1000]), min_size=1), number_list2=st.lists(elements=st.sampled_from([1, 5, 10, 50, 100, 500, 1000]), min_size=1))
+def test_returns_different_roman_numerals_for_different_lists_of_numbers(number_list1, number_list2):
+    assume(number_list1 != number_list2)
+    assert convert_number_list_to_stack(number_list1) != convert_number_list_to_stack(number_list2)
